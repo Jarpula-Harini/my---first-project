c96271aaea483b140017c428c1e89946fbabb63d
Jarpula Harini <sirih023@gmail.com>
2022-04-25 18:15:05 +0530

    This is my first addNumber code

---
+++ src/main/java/com/dlithe/banking/app/practice/Demo.java
@@ -0,0 +1,20 @@
+package com.dlithe.banking.app.practice;
+
+
+/**
+ * @author Harini
+ * @version 1.8
+ * Date 25/04/2022
+ *
+ */
+
+public class Demo {
+
+    public static void addNumbers(){
+        int a = 5;
+        int b = 2;
+        System.out.println(a+b);
+
+
+    }
+}
